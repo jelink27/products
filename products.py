b1fa74a54307ef3b834cbebf2a8826712d850a45
Jelink <jelink27@gmail.com>
2021-10-06 16:59:17 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:    #在不知道會執行幾次的時候通常用while
     if name == 'q': #quit
         break
     price = input('請輸入商品價格:')
+    price = int(price) #price轉換成整數 但轉換後+法不能合併
     #p = [] #這3行也可以寫成p = [name, price]
     #p.append(name) #把name裝進p裡面
     #p.append(price) #把price裝進p裡面
@@ -16,7 +17,10 @@ print(products)
 for p in products:
     print(p[0],'的價格是',p[1]) #每一個清單的第0格存名稱 1存價格
 
-with open('product.csv', 'w') as f: #打開這個檔案 寫入product.csv檔 如果沒有會創建 如果有會覆蓋
+with open('product.csv', 'w', encoding='utf-8') as f: #打開這個檔案 寫入product.csv檔 如果沒有會創建 如果有會覆蓋
+    f.write('商品,價格\n')
     for p in products:              #with在使用完後會自動關閉，不會夾住檔案
-        f.write(p[0] + ',' + p[1] + '\n') #用.write 寫進f裡面 通常最後會用換行做合併 在csv裡面才會換行
+        f.write(p[0] + ',' + str(p[1]) + '\n') #用.write 寫進f裡面 通常最後會用換行做合併 在csv裡面才會換行
+        #前面多了把price轉換成數字 數字不能跟字串合併 必須轉換成str字串
 
+#with open('product.csv', 'w', encoding='utf-8') as f: #後面那行意思為 寫入這個檔案用的編碼為utf-8
